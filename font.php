12bef4716565dfa2b156ce87fecd9972a9c6c890
sukultabii <98345170+sukultabii@users.noreply.github.com>
2022-02-24 09:10:14 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("baozi.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("baozi.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นายยิ่งคุณ รอดทิม เลขที่ 1 ห้อง ม.4/14 เลขประจำตัว 88888</h1>
+    <h1 id="font2">นายยิ่งคุณ รอดทิม เลขที่ 1 ห้อง ม.4/14 เลขประจำตัว 88888</h1>
+  </body>
+</html>
