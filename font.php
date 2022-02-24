bad9343f2fcb8613fb8521485c182be3b35a3c91
sukultabii <98345170+sukultabii@users.noreply.github.com>
2022-02-24 09:13:57 +0700

    Update font.php

--- font.php
+++ font.php
@@ -4,7 +4,7 @@
     <style>
       @font-face{
         font-family: "MyFont1";
-        src: url("");
+        src: url("MiPancake.ttf");
       }
       @font-face{
         font-family: "MyFont2";
