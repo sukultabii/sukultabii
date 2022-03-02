22a5f7f5a90545170e0beded1c479b0d63d55775
sukultabii <98345170+sukultabii@users.noreply.github.com>
2022-03-02 13:46:37 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,6 +15,11 @@
         <td>จอน จองกุก</td>
         <td><img src="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcSB3TP5MlzEzYk9DvABQc8OUCM9LQSSYgiUv4OirbvRgKzmKouU&psig=AOvVaw2OJGjY8UQh3-Dy9edfmjcv&ust=1646288684384000&source=images&cd=vfe&ved=0CAwQjhxqFwoTCNCE0LPlpvYCFQAAAAAdAAAAABBE" width="45px"></td>
         <td><a href="https://twitter.com/BTS_twt">Twitter</a></td>
+      <tr>
+        <th>2</th>
+        <th>คิม จีซู</th>
+         <td><img src="https://www.brighttv.co.th/entertain/%e0%b9%88jisoo-blackpink-yg" width="45px"></td>
+        <td><a href="https://twitter.com/blackpink">Twitter</a></td> 
       </tr>
     </table>
   </body>
