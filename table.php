5f314c6a0ab7f0d8bae5f36aef41b20b895bc180
sukultabii <98345170+sukultabii@users.noreply.github.com>
2022-03-02 14:22:51 +0700

    Update table.php

--- table.php
+++ table.php
@@ -12,14 +12,22 @@
       </tr>
       <tr>
         <td>1</td>
-        <td>จอน จองกุก</td>
+        <td>จองกุก</td>
         <td><img src="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcSB3TP5MlzEzYk9DvABQc8OUCM9LQSSYgiUv4OirbvRgKzmKouU&psig=AOvVaw2OJGjY8UQh3-Dy9edfmjcv&ust=1646288684384000&source=images&cd=vfe&ved=0CAwQjhxqFwoTCNCE0LPlpvYCFQAAAAAdAAAAABBE" width="45px"></td>
         <td><a href="https://twitter.com/BTS_twt">Twitter</a></td>
+      </tr>
       <tr>
         <th>2</th>
-        <th>คิม จีซู</th>
-         <td><img src="https://www.brighttv.co.th/entertain/%e0%b9%88jisoo-blackpink-yg" width="45px"></td>
+        <th>จีซู</th>
+        <td><img src="https://www.brighttv.co.th/entertain/%e0%b9%88jisoo-blackpink-yg" width="45px"></td>
         <td><a href="https://twitter.com/blackpink">Twitter</a></td> 
+      </tr>
+      <tr>
+        <th>2</th>
+        <th>ฮารุโตะ</th>
+        <td><img src="https://www.pinkvilla.com/imageresize/haruto_main.png?width=752&format=webp&t=pvorg" width="45px"></td>
+        <td><a href="https://twitter.com/treasuremembers">Twitter</a></td> 
+      </tr>
       </tr>
     </table>
   </body>
