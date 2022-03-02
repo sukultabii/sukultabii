2ce365be80b2f50aaf15c459ed590436b04538ec
sukultabii <98345170+sukultabii@users.noreply.github.com>
2022-03-02 13:39:57 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,7 +13,7 @@
       <tr>
         <td>1</td>
         <td>จอน จองกุก</td>
-        <td><img src="https://images.app.goo.gl/FX7CAXwYMGxJdznVA"width="45px"></td>
+        <td><img src="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcSB3TP5MlzEzYk9DvABQc8OUCM9LQSSYgiUv4OirbvRgKzmKouU&psig=AOvVaw2OJGjY8UQh3-Dy9edfmjcv&ust=1646288684384000&source=images&cd=vfe&ved=0CAwQjhxqFwoTCNCE0LPlpvYCFQAAAAAdAAAAABBE" width="45px"></td>
         <td><a href="https://twitter.com/BTS_twt">Twitter</a></td>
       </tr>
     </table>
