2bae77471be5fd68f5845bdc3893180c531a2b72
sukultabii <98345170+sukultabii@users.noreply.github.com>
2022-03-02 10:14:00 +0700

    Create mypage1.php

---
+++ mypage1.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>เว็บไซต์ของ สุกุลตรา บุญเรือง</title>
+  </head>
+  <body>
+    <a href="https://facebook.com"><h1>Facebook</h1></a>
+    <a href="mypage2.php"><h1>Mypage2</h1></a>
+  </body>
+</html>
